c40c3141ec12e77a301f7ce7f48af9e4dbb540c1
Азат Янбердин <azat@MacBook-Air.local>
2025-09-06 15:12:47 +0500

    add new update, get requests

--- petstore_client.py
+++ petstore_client.py
@@ -18,8 +18,14 @@ class PetstoreClient:
     def get_pet(self, pet_id):
         return self.http_client.get('GET', f'/pet/{pet_id}')
     
-    def update_pet(self):
-        return self.http_client.put('PUT', '/pet')
+    def update_pet(self, pet_data=None):
+        if pet_data is None:
+            pet_data = {
+                "id": 1,
+                "name": "updated_test_pet",
+                "status": "sold"
+            }
+        return self.http_client.put('PUT', '/pet', data=pet_data)
     
     def delete_pet(self, pet_id):
         return self.http_client.delete('DELETE', f'/pet/{pet_id}')
--- test_petstore.py
+++ test_petstore.py
@@ -3,7 +3,7 @@ from petstore_client import PetstoreClient
 
 
 def test_add_pet():
-    http_client = HttpClient('https://petstore.swagger.io/v2//')
+    http_client = HttpClient('https://petstore.swagger.io/v2/')
     petstore_client = PetstoreClient(http_client)
     response = petstore_client.add_pet()
     assert response.status_code == 200
@@ -13,14 +13,17 @@ def test_add_pet():
 def test_get_pet():
     http_client = HttpClient('https://petstore.swagger.io/v2/')
     petstore_client = PetstoreClient(http_client)
-    # Просто проверяем, что API отвечает на GET запрос
     response = petstore_client.get_pet(1)
-    # Ожидаем либо 200 (питомец найден), либо 404 (питомец не найден)
     assert response.status_code in [200, 404]
 
 
 def test_update_pet():
-    pass
+    http_client = HttpClient('https://petstore.swagger.io/v2/')
+    petstore_client = PetstoreClient(http_client)
+    response = petstore_client.update_pet()
+    assert response.status_code == 200
+    
+    
 
 
 def test_delete_pet():
