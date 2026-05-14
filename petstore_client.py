1c39d17664662cda903ab6d06d35991fa5b0de21
azat <azatanberdin02@gmail.com>
2026-05-14 17:12:36 +0500

    add petstoreclient

--- petstore_client.py
+++ petstore_client.py
@@ -3,19 +3,22 @@ from requests import Response
 from http_client import HttpClient
 
 
-class PetstoreClient:
-    
-    def __init__(self, http_client: HttpClient):
-        self.http_client: HttpClient = http_client
-    
+class PetstoreClient(HttpClient):
+
+    def __init__(self, url: str = 'https://petstore.swagger.io/v2/'):
+        super().__init__(url)
+
     def add_pet(self, pet_data: Dict[str, Any]) -> Response:
-        return self.http_client.post('pet', json=pet_data)
-    
+        return self.post('pet', json=pet_data)
+
     def get_pet(self, pet_id: int) -> Response:
-        return self.http_client.get(f'pet/{pet_id}')
-    
+        return self.get(f'pet/{pet_id}')
+
     def update_pet(self, pet_data: Dict[str, Any]) -> Response:
-        return self.http_client.put('pet', json=pet_data)
-    
+        return self.put('pet', json=pet_data)
+
     def delete_pet(self, pet_id: int) -> Response:
-        return self.http_client.delete(f'pet/{pet_id}')
+        return self.delete(f'pet/{pet_id}')
+
+    def find_by_status(self, status: str) -> Response:
+        return self.get(f'pet/findByStatus?status={status}')
